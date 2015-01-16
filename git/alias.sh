8f43db29da24b31fbfe747cf678efdf6f42f4661
customcommander <hello@spinjs.com>
2015-01-16 17:50:59 +0000

    add some more git aliases

--- git/alias.sh
+++ git/alias.sh
@@ -2,3 +2,8 @@
 
 git config --global alias.lg1 "log --format=oneline"
 git config --global alias.st "status"
+git config --global alias.pulr "pull --rebase"
+git config --global alias.b "branch"
+
+# Push to Gerrit for code review
+git config --global alias.pug "push origin HEAD:refs/for/master"
