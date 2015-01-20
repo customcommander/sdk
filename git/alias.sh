4f5671203fdb1349f5acda90b915bcae3cf26fd7
customcommander <hello@spinjs.com>
2015-01-20 10:41:28 +0000

    add cia as a git alias for ammending the last commit

--- git/alias.sh
+++ git/alias.sh
@@ -4,6 +4,7 @@ git config --global alias.lg1 "log --format=oneline"
 git config --global alias.st "status"
 git config --global alias.pulr "pull --rebase"
 git config --global alias.b "branch"
+git config --global alias.cia "commit --amend"
 
 # Push to Gerrit for code review
 git config --global alias.pug "push origin HEAD:refs/for/master"
