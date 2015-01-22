bd664563ce9039757b93e33973fe1dd22e0f7b73
customcommander <hello@spinjs.com>
2015-01-22 10:54:00 +0000

    add a global gitignore file

--- git/config_global.sh
+++ git/config_global.sh
@@ -8,3 +8,8 @@ git config --global alias.cia "commit --amend"
 
 # Push to Gerrit for code review
 git config --global alias.pug "push origin HEAD:refs/for/master"
+
+# Setup a file that contains exclude patterns for every git repositories
+# on this computer. This means that you don't have to manually add '.DS_Store'
+# to every single '.gitignore' files.
+git config --global core.excludesfile ~/.gitignore_global
