d7188e4948955a7359eff27cb0e6358d51939827
customcommander <hello@spinjs.com>
2015-04-11 09:39:12 +0100

    remove git pulr alias - wont be used very much

--- git/config_global.sh
+++ git/config_global.sh
@@ -3,7 +3,6 @@
 # git lg1 format is: 'short_hash title (author, commit_date)'
 git config --global alias.lg1 "log --format='%Cred%h %Creset%s %Cblue(%an, %cr)'"
 git config --global alias.st "status"
-git config --global alias.pulr "pull --rebase"
 git config --global alias.b "branch"
 git config --global alias.cia "commit --amend"
 git config --global alias.com "checkout master"
